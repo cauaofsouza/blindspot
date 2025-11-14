cd5a983171dd3ec5712025582b3254aa8477c940
cauaof_souza <cauasantos064@gmail.com>
2025-11-14 10:04:16 -0300

    projeto criado

---
+++ src/main/java/com/cauaofsouza/blindspot/BlindspotApplication.java
@@ -0,0 +1,13 @@
+package com.cauaofsouza.blindspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindspotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlindspotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cauaofsouza/blindspot/BlindspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauaofsouza.blindspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindspotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
